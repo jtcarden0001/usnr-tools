f93a2031fd275beb00ed22564f0631927789d69e
John Carden <john.carden.02@gmail.com>
2025-01-20 23:30:38 +0000

    added openpyxl and updated requirements.txt

--- nrrm-processor/.devcontainer/postCreateCommand.sh
+++ nrrm-processor/.devcontainer/postCreateCommand.sh
@@ -1,3 +1,5 @@
 pip install psycopg[binary] # postgres database connector library
 pip install pandas # data manipulation library
+pip install openpyxl # excel file manipulation library
 pip install pytest # testing library
+pip freeze > requirements.txt # save the installed libraries to a file
